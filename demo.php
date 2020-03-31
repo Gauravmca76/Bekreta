b2a455e885a1e196fe2e9d767f104e0df36311f8
Gauravmca76 <35734722+Gauravmca76@users.noreply.github.com>
2020-03-31 12:45:22 +0530

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,50 @@
+<?php
+$tir=20;
+$tir1=10;
+$tp=70;
+$dataPoints = array( 
+	array("label"=>"Principal", "y"=>$tp),
+	array("label"=>"Interest", "y"=>$tir),
+	array("label"=>"Interest12", "y"=>$tir1))
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JQM Charts</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes"/>
+	<link rel="stylesheet" href="./jquery.mobile/demos/css/themes/default/jquery.mobile-1.4.5.min.css">
+	<link rel="stylesheet" href="./jquery.mobile/demos/_assets/css/jqm-demos.css">
+	<link rel="shortcut icon" href="./favicon.ico">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<script src="./jquery.mobile/demos/js/jquery.js"></script>
+	<script src="./jquery.mobile/demos/_assets/js/index.js"></script>
+	<script src="./jquery.mobile/demos/js/jquery.mobile-1.4.5.min.js"></script>
+	<script>
+window.onload = function() { 
+var chart = new CanvasJS.Chart("mycanvas", {
+	animationEnabled: true,
+	title: {
+		text: "EMI Calculation Pie Chart"
+	},
+	data: [{
+		type: "pie",
+		indexLabel: "{label} ({y})",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+}
+</script>
+</head>
+<body>
+<div id="pagePieChart" data-role="page" data-rockncoder-jspage="managePieChart">
+		<header data-role="header" data-position="fixed">
+			<h1>Pie Chart</h1>
+			<div id="mycanvas" style="height: 370px;width: 100%;"></div>
+		</header>
+		<a href="index.php">Next</a>
+	</div>
+</body>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</html>
