ef6a5b257a3a17e5cc63d99d38c5413e05a52e2d
Gauravmca76 <35734722+Gauravmca76@users.noreply.github.com>
2020-04-16 17:44:36 +0530

    Update newfinancial.php

--- newfinancial.php
+++ newfinancial.php
@@ -12,7 +12,7 @@
       <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div data-role="page">
+<div data-role="page" style="background-color:#ddd;">
 		<div data-role = "header">
             <div class = "ui-body-a ui-body">
                <img src="logoimages/bekreta.png" width="100" height="40">
@@ -74,14 +74,10 @@
                         include('db.php');
                         $sql="SELECT * FROM sales WHERE id = 5";
                         $result=mysqli_query($conn,$sql);
-                        $n=mysqli_num_rows($resul1);
-                        if($n > 0)
-                        {
-                              $row=mysqli_fetch_array($result);
-                              $nr=$row['ds'] + $row['chs'];
-                              $nr1=($row['ds1'] + $row['chs1']);
-                              $nr2=($row['ds2'] + $row['chs2']);
-                        }
+                        $row=mysqli_fetch_array($result);
+                        $nr=$row['ds'] + $row['chs'];
+                        $nr1=($row['ds1'] + $row['chs1']);
+                        $nr2=($row['ds2'] + $row['chs2']);
                      ?>
                      <tr>
                         <td><a href="sales.php?planid="><button class="ui-btn ui-btn-b ui-shadow ui-corner-all ui-btn-icon-left ui-icon-edit ui-btn-icon-notext"></button></a></td>
@@ -92,8 +88,93 @@
                      </tr>
                   </tbody>
                   </table>
-      			</div>
-      			<input type="submit" name="nxt_submit" value="Submit & Download" data-inline="true">
+      			</div><!-- card end-->
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <div class="card2">
+            <h3>Expenses<a href="#popupInfo1" data-rel="popup" data-transition="pop" class="ui-btn ui-btn-inline ui-icon-info ui-btn-icon-notext" title="Learn more" style="background-color: #ddd">Learn more</a>
+            <a href="#popupExpenses" data-rel="popup" data-position-to="window" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-plus ui-btn-icon-left ui-btn-b" data-transition="pop" style="margin-left: 37px;">Add Expenses</a></h3>
+            <div data-role="popup" id="popupInfo1" class="ui-content" data-theme="a" style="max-width:350px;">
+                    <p>Expenditure is an outflow of money to another person or group to pay for an item or service, or for a category of costs. For a tenant, rent is an expense. For students or parents, tuition is an expense. Buying food, clothing, furniture or an automobile is often referred to as an expense.</p>
+               </div>
+            <button type="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-edit ui-btn-icon-notext edit-row1" style="margin-left: 250px;"></button>
+      <button type="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-delete ui-btn-icon-notext delete-row1" style="margin-left: 300px; margin-top: -40px;"></button>
+   <div data-role="popup" id="popupExpenses" data-theme="a" class="ui-corner-all" style="width:300px; height:500px; background-color:#ddd;">
+                        <h3 align="center"><B>Expenses</B></h3>
+                        <label for="select_expense" class="select">Expenses</label>
+                        <select id="select_expense">
+                              <option value="--Select--">--Select--</option>
+                              <option value="Purchased Good">Purchesed Good</option>
+                              <option value="Service">Service</option>
+                              <option value="Payroll">Payroll</option>
+                              <option value="Wages">Wages</option>
+                              <option value="Depercision">Derpercision</option>
+                              <option value="Intrest">Intrest</option>
+                              <option value="Tax">Tax</option>
+                              <option value="Rent & Misc">Rent & Misc</option>
+                              <option value="Raw Material">Raw Material</option>
+                              <option value="Professional Service">Professional Service</option>
+                              <option value="Utility Expenses">Utility Expenses</option>
+                              <option value="Administartion Expenses">Administration Expenses</option>
+                              <option value="Implementation Losses">Implementation Losses</option>
+                              <option value="Other Expenses">Other Expenses</option>
+                              <option value="Other">Other</option>
+                        </select>
+                        <input type="text" id="nametxtex" placeholder="Enter Income Name" style="display: none;">
+                        <label for="year1">Year 1 Amount</label>
+                        <input type="text" id="eyear1" value="" placeholder="Enter Amount" data-theme="a">
+                        <label for="year2">Year 2 Amount</label>
+                        <input type="text" id="eyear2" value="" placeholder="Enter Amount" data-theme="a">
+                        <label for="year3">Year 3 Amount</label>
+                        <input type="text" id="eyear3" value="" placeholder="Enter Amount" data-theme="a">
+                        <button type="button" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-plus add-row">Add Expense</button>
+                 </div>
+                <table data-role="table" id="expexpenses" class="ui-responsive table-stroke">
+                <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Expenses Name</th>
+                  <th>Year 1</th>
+                  <th>Year 2</th>
+                  <th>Year 3</th>
+               </tr>
+                </thead>
+                <tbody>
+                <?php
+                  error_reporting(0);
+                  include('db.php');
+                  $sql2="SELECT * FROM sales WHERE id = 5";
+                  $result2=mysqli_query($conn,$sql2);
+                  $num=mysqli_num_rows($result2);
+                  if($num > 0)
+                  {
+                     $row2=mysqli_fetch_assoc($result2);
+                     $sval1=json_decode($row2['sy1']);
+                     $sval2=json_decode($row2['sy2']);
+                     $sval3=json_decode($row2['sy3']);
+                  }
+                  $i=0;
+                  while($i <= sizeof($sval1))
+                  {
+                     $market+=$sval1[$i];
+                     $market1+=$sval2[$i];
+                     $market2+=$sval3[$i];
+                     $i++;
+                  }
+                ?>
+                <tr>
+                <td><a href="sales.php?planid="><button class="ui-btn ui-btn-b ui-shadow ui-corner-all ui-btn-icon-left ui-icon-edit ui-btn-icon-notext"></button></a></td>
+                <td><input type="hidden" name="mname" value="Marketing Expense">Marketing Expense</td>
+                <td><input type="hidden" name="mval1" value="<?=$market?>"><?=$market?></td>
+                <td><input type="hidden" name="mval2" value="<?=$market1?>"><?=$market1?></td>
+                <td><input type="hidden" name="mval3" value="<?=$market2?>"><?=$market2?></td>
+                </tr>
+                </tbody>
+                </table>
+         </div><!-- card end-->
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div class="card2">
+            </div>
+      			<input type="submit" name="download_submit" value="Submit & Download" data-inline="true">
       		</form>
       	</div>
 </div>
